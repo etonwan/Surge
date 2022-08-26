93cc6475a64421f3f684a0168f743d5f27e70ab7
Kurt Wan <93763799+kurt-wan@users.noreply.github.com>
2022-08-26 17:17:53 +0800

    Create IP-Check.js

---
+++ IP-Check.js
@@ -0,0 +1,36 @@
+/*
+ * 由@congcong0806编写
+ * 原脚本地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
+ * 由@Rabbit-Spec修改
+ * 更新日期：2022.08.14
+ * 版本：1.5
+ */
+
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "节点信息",
+    content: `IP：${ip}\nISP：${isp}\nLocation：${emoji}${country} - ${city}`,
+    icon: "antenna.radiowaves.left.and.right"
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+      if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+
+}
