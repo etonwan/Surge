0b5f6a1a3e63e7d92719e6709fbb3dda41084d43
Eton Wan <93763799+etonwan@users.noreply.github.com>
2023-02-23 14:48:05 +0800

    Update stream-all.js

--- stream-all.js
+++ stream-all.js
@@ -1,11 +1,3 @@
-/*
- * 由@LucaLin233编写
- * 原脚本地址：https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/stream-all.js
- * 由@Rabbit-Spec修改
- * 更新日期：2022.07.22
- * 版本：2.3
- */
-
 const REQUEST_HEADERS = {
     'User-Agent':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
@@ -44,7 +36,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="Disney+: 已解锁 ➟ "+region.toUpperCase()
+        disney_result="Disney+: 已解锁，区域: "+region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
@@ -103,7 +95,7 @@ panel_result['content'] = content
         if (code === 'Not Available') {
           youtube_check_result += '不支持解锁'
         } else {
-          youtube_check_result += '已解锁 ➟ ' + code.toUpperCase()
+          youtube_check_result += '已解锁，区域: ' + code.toUpperCase()
         }
       })
       .catch((error) => {
@@ -154,12 +146,12 @@ panel_result['content'] = content
   
     let netflix_check_result = 'Netflix: '
   
-    await inner_check(81215567)
+    await inner_check(80062035)
       .then((code) => {
         if (code === 'Not Found') {
           return inner_check(80018499)
         }
-        netflix_check_result += '已完整解锁 ➟ ' + code.toUpperCase()
+        netflix_check_result += '已完整解锁，区域: ' + code.toUpperCase()
         return Promise.reject('BreakSignal')
       })
       .then((code) => {
@@ -167,7 +159,7 @@ panel_result['content'] = content
           return Promise.reject('Not Available')
         }
   
-        netflix_check_result += '仅解锁自制剧 ➟ ' + code.toUpperCase()
+        netflix_check_result += '仅解锁自制剧，区域: ' + code.toUpperCase()
         return Promise.reject('BreakSignal')
       })
       .catch((error) => {
