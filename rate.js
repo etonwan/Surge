109933d906a7e09b7ec102fba743d3e2f50594d5
Eton Wan <93763799+etonwan@users.noreply.github.com>
2023-08-14 03:36:14 +0800

    Update rate.js

--- rate.js
+++ rate.js
@@ -19,7 +19,7 @@ $httpClient.get(url, function(error, response, data) {
     hour12: false
   });
 
-  const content = `1USD = ${cnyToTry}CNY
+  const content = `1USD - ${cnyToTry}CNY
 🇨🇳1人民币兑换 ${cnyToTry}🇹🇷里拉
 🇨🇳1人民币兑换 ${cnyToHkd}🇭🇰港币
 🇨🇳1人民币兑换 ${cnyToJpy}🇯🇵日元
